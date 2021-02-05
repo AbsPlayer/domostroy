96083b34c8178bcb652ca2b5eb5461646e859530
AbsPlayer <absplayer@ua.fm>
2021-02-05 10:06:42 +0200

    corrected - testing for incorrect inputted URL zhk

--- domostroy_query_zhk.py
+++ domostroy_query_zhk.py
@@ -1,10 +1,20 @@
 import domostroy
+from urllib.parse import urlparse
 
 def query_zhk():
     city_name = input("Введите название города: ") # город
     zhk_name = input("Введите название ЖК: ") # название ЖК
     url_zhk = input("Введите ссылку на ЖК: ") # ссылка на ЖК
     # все введенные данные будут использованы в xls-отчёте
+
+    msg = "Введенный URL некорректный или не может быть спаршен"
+    cities = domostroy.get_site_urls()
+    urls = [i[1] for i in cities.values()]
+    up = urlparse(url_zhk)
+    url_ = up[0] + "://" + up[1]
+    if url_ not in urls:
+        print(msg)
+        quit()
 
     cities = {}
     zhks = {}
